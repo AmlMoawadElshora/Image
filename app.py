570161205648f50ec160850f5324ff5ca45bab13
AmlMoawadElshora <ineedtorun127@gmail.com>
2024-01-30 20:37:30 +0200

    files update

--- app.py
+++ app.py
@@ -1,59 +1,88 @@
-import streamlit as st
+from flask import Flask, render_template, request, jsonify
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
-
 import torch
 from torchvision.models.detection import fasterrcnn_resnet50_fpn_v2, FasterRCNN_ResNet50_FPN_V2_Weights
 from torchvision.utils import draw_bounding_boxes
+import io
+
+app = Flask(__name__)
 
 weights = FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT
-categories = weights.meta["categories"] ## ['__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stopsign',]
-img_preprocess = weights.transforms() ## Scales values from 0-255 range to 0-1 range.
+categories = weights.meta["categories"]
+img_preprocess = weights.transforms()
 
-@st.cache_resource
 def load_model():
     model = fasterrcnn_resnet50_fpn_v2(weights=weights, box_score_thresh=0.5)
-    model.eval(); ## Setting Model for Evaluation/Prediction   
+    model.eval()
     return model
 
 model = load_model()
 
 def make_prediction(img): 
-    img_processed = img_preprocess(img) ## (3,500,500) 
-    prediction = model(img_processed.unsqueeze(0)) # (1,3,500,500)
-    prediction = prediction[0]                       ## Dictionary with keys "boxes", "labels", "scores".
+    img_processed = img_preprocess(img)
+    prediction = model(img_processed.unsqueeze(0))
+    prediction = prediction[0]
     prediction["labels"] = [categories[label] for label in prediction["labels"]]
     return prediction
 
-def create_image_with_bboxes(img, prediction): ## Adds Bounding Boxes around original Image.
-    img_tensor = torch.tensor(img) ## Transpose
+def create_image_with_bboxes(img, prediction):
+    img_tensor = torch.tensor(img)
     img_with_bboxes = draw_bounding_boxes(img_tensor, boxes=prediction["boxes"], labels=prediction["labels"],
                                           colors=["red" if label=="person" else "green" for label in prediction["labels"]] , width=2)
-    img_with_bboxes_np = img_with_bboxes.detach().numpy().transpose(1,2,0) ### (3,W,H) -> (W,H,3), Channel first to channel last.
+    img_with_bboxes_np = img_with_bboxes.detach().numpy().transpose(1, 2, 0)
     return img_with_bboxes_np
 
-## Dashboard
-st.title("Object Detector :tea: :coffee:")
-upload = st.file_uploader(label="Upload Image Here:", type=["png", "jpg", "jpeg"])
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            img = Image.open(io.BytesIO(file.read()))
+            prediction = make_prediction(img)
+            img_with_bbox = create_image_with_bboxes(np.array(img).transpose(2, 0, 1), prediction)
 
-if upload:
-    img = Image.open(upload)
+            fig = plt.figure(figsize=(12, 12))
+            ax = fig.add_subplot(111)
+            plt.imshow(img_with_bbox)
+            plt.xticks([], [])
+            plt.yticks([], [])
+            ax.spines[["top", "bottom", "right", "left"]].set_visible(False)
 
-    prediction = make_prediction(img) ## Dictionary
-    img_with_bbox = create_image_with_bboxes(np.array(img).transpose(2,0,1), prediction) ## (W,H,3) -> (3,W,H)
+            # Save the figure to a bytes object
+            img_bytes = io.BytesIO()
+            plt.savefig(img_bytes, format='png')
+            img_bytes.seek(0)
 
-    fig = plt.figure(figsize=(12,12))
-    ax = fig.add_subplot(111)
-    plt.imshow(img_with_bbox)
-    plt.xticks([],[])
-    plt.yticks([],[])
-    ax.spines[["top", "bottom", "right", "left"]].set_visible(False)
+            plt.close(fig)
 
-    st.pyplot(fig, use_container_width=True)
+            del prediction["boxes"]
+            predicted_probabilities = str(prediction)
+            return render_template('result.html', img_data=img_bytes.read(), predicted_probabilities=predicted_probabilities)
 
-    del prediction["boxes"]
-    st.header("Predicted Probabilities")
-    st.write(prediction)
+    return render_template('index.html')
 
+@app.route('/predictApi', methods=["POST"])
+def predict_api():
+    try:
+        if 'file' not in request.files:
+            return jsonify({'error': 'Please try again. The Image does not exist'})
 
+        image = request.files.get('file')
+        img = Image.open(io.BytesIO(image.read()))
+        prediction = make_prediction(img)
+
+        # Adjust this part based on your desired JSON response
+        response_data = {
+            'labels': prediction['labels'],
+            'scores': prediction['scores'].tolist()
+        }
+
+        return jsonify(response_data)
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
